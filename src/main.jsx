4de85cfcf9e3cee18ad31d52564d1aa405da2029
Abhijeet.maity <Abhijeet.maity@nimapinfotech.in>
2025-03-11 11:01:32 +0530

    added User Provider context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./Contexts/ThemeProvider.jsx";
+import { UserProvider } from "./Contexts/UserProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
+  <UserProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </UserProvider>
 );
